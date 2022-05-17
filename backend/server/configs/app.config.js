4a8b9c76beb204bdf54dbc22654e8497923456c2
dragon1211 <vikt0r1211@hotmail.com>
2022-05-17 20:54:30 +0900

    fixed m

--- backend/server/configs/app.config.js
+++ backend/server/configs/app.config.js
@@ -2,7 +2,6 @@ export const APP_CONFIG = {
   secret_private_key: "okaimono-secret-key",
   owner_private_key: "0x9382719032840ea820",
   support_mail: "support@fantation-coin.com",
-  FRONT_URL: "http://localhost:3000"
 };
 
 export const FRONT_URL = "http://localhost:3000";
