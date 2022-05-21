57d30f1bad763ca1065338dfa054d0b66154aa5c
dragon1211 <vikt0r1211@hotmail.com>
2022-05-21 16:09:42 +0900

    fixed fe

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { API_URL } from "configs/AppConfig";
 
-class UserService{
-  getCurrentUser() {
+const UserService = {};
+
+UserService.getCurrentUser = function() {
     try {
       return JSON.parse(localStorage.getItem('auth_token'));
     }
@@ -10,9 +11,9 @@ class UserService{
       console.log("getCurrentUser ", ex)
       return null
     }
-  }
+}
 
-  setCurrentUser(data) {
+UserService.setCurrentUser = function (data) {
     let old_user = this.getCurrentUser();
     if(data.password) delete data.password;
     let user = data;
@@ -20,19 +21,18 @@ class UserService{
       user = {...data, "accessToken": old_user.accessToken}
     }
     localStorage.setItem("auth_token", JSON.stringify(user));
-  }
+}
 
   
   //-----------Profile Management----------//
-  async updatePersonalInfo(personalObj) {
+UserService.updatePersonalInfo = async function(personalObj) {
     return await axios
       .put(`${API_URL}/api/user/update/personal-info`, personalObj)
-  }
-
-  async updateUserAvatar(obj) {
-    return await axios
-    .put(`${API_URL}/api/user/update/avatar`, obj)
-  }
 }
 
-export default new UserService();
+UserService.updateUserAvatar = async function(obj) {
+    return await axios
+    .put(`${API_URL}/api/user/update/avatar`, obj)
+}
+
+export default UserService;
